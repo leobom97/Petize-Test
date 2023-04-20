600616e2740ca76b13321345267a42e5dd15ee08
Leonardo <leordoria2016@gmail.com>
2023-04-20 09:35:43 -0300

    (feat):"Subindo projeto completo"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubApiService } from './service/github-api.service';
+import { UserListComponent } from './user-list/user-list.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FilterPipe } from './filter.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, UserListComponent, FilterPipe],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [GithubApiService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
